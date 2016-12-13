1907436bd74ebb71a3d287e6f679103a9114cffc
OdinetsNastya <nastya_odinets@mail.ru>
2016-12-13 13:23:44 +0200

    changed

--- WebApplication1/GuestResponse.cs
+++ WebApplication1/GuestResponse.cs
@@ -8,8 +8,7 @@ namespace WebApplication1
     
         public class GuestResponse
         {
-            public string Name { get; set; }
-            public string Sum { get; set; }
+                        public string Sum { get; set; }
             public string Phone { get; set; }
             public bool? WillAttend { get; set; }
         }
